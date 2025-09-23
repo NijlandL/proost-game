f07f04d5d48ad88b1db8c6bcc6025fdee78f81eb
NijlandL <46446003+NijlandL@users.noreply.github.com>
2025-09-23 23:41:46 +0200

    Fix babel configuration and build errors

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,4 @@ module.exports = function(api) {
   };
 };
       useESModules: false
-    }]
-  ]
-};
+    
--- metro.config.js
+++ metro.config.js
@@ -2,9 +2,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Add web platform support
 config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 
+module.exports = config;
 module.exports = config;
 config.resolver.alias = {
   '@firebase/app': '@firebase/app',
