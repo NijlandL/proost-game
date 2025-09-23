f7c0950edcfed1ac4d10f646eff36212de9ba193
NijlandL <46446003+NijlandL@users.noreply.github.com>
2025-09-23 23:45:42 +0200

    Fix Babel compatibility issues for Netlify deployment

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,9 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-
 config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 
+module.exports = config;
 module.exports = config;
 module.exports = config;
 config.resolver.alias = {
